dd2fb6f1d6b2d46208f62be4dc7f10dbdbd17c65
David Ricardo <davito0203@gmail.com>
2019-11-24 15:28:14 +0400

    3D Cylinder

---
+++ 3D/Cylinder.py
@@ -0,0 +1,28 @@
+import meep as mp
+import numpy as np
+import math
+
+cell_size = mp.Vector3(2,2,2)
+
+geometry=[#mp.Block(center=mp.Vector3(0,0,0), size=cell_size, material=mp.air),
+          mp.Cylinder(radius=1, material=mp.metal, height=1.9, center=mp.Vector3(0,0,0))]
+
+sim = mp.Simulation(resolution=50, cell_size=cell_size, geometry=geometry)
+
+sim.init_sim()
+
+eps_data = sim.get_epsilon()
+#eps_data = sim.get_array()
+
+
+#sim.plot3D(eps_data)
+
+#from mayavi import mlab
+#s = mlab.contour3d()
+#mlab.show()
+#mlab.savefig(filename='test2.png')
+
+from mayavi import mlab
+s = mlab.contour3d(eps_data, colormap="hsv")
+#mlab.show()
+mlab.savefig('Cylinder3D.pdf')
