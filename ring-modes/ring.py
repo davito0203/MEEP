7778a93c3dae063d8703cf284f4b57f56313fa4f
David Ricardo <davito0203@gmail.com>
2020-03-03 10:49:30 +0400

    Modes of a Ring Resonator with plot and videos

--- ring-modes/ring.py
+++ ring-modes/ring.py
@@ -1,47 +1,98 @@
-# Calculating 2d ring-resonator modes, from the Meep tutorial.
-from __future__ import division
-
 import meep as mp
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import Video
 
-def main():
-    n = 3.4                 # index of waveguide
-    w = 1                   # width of waveguide
-    r = 1                   # inner radius of ring
-    pad = 4                 # padding between waveguide and edge of PML
-    dpml = 2                # thickness of PML
-    sxy = 2*(r+w+pad+dpml)  # cell size
+n = 3.4                 # index of waveguide
+w = 1                   # width of waveguide
+r = 1                   # inner radius of ring
+pad = 4                 # padding between waveguide and edge of PML
+dpml = 2                # thickness of PML
+sxy = 2*(r+w+pad+dpml)  # cell size
 
-    # Create a ring waveguide by two overlapping cylinders - later objects
-    # take precedence over earlier objects, so we put the outer cylinder first.
-    # and the inner (air) cylinder second.
+c1 = mp.Cylinder(radius=r+w, material=mp.Medium(index=n))
+c2 = mp.Cylinder(radius=r)
 
-    c1 = mp.Cylinder(radius=r+w, material=mp.Medium(index=n))
-    c2 = mp.Cylinder(radius=r)
 
-    # If we don't want to excite a specific mode symmetry, we can just
-    # put a single point source at some arbitrary place, pointing in some
-    # arbitrary direction.  We will only look for Ez-polarized modes.
+fcen = 0.15              # pulse center frequency
+df = 0.1                 # pulse frequency width
+src = mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r+0.1))
 
-    fcen = 0.15             # pulse center frequency
-    df = 0.1                # pulse width (in frequency)
 
-    src = mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r+0.1))
 
-    sim = mp.Simulation(cell_size=mp.Vector3(sxy, sxy),
-                        geometry=[c1, c2],
-                        sources=[src],
-                        resolution=100,
-                        symmetries=[mp.Mirror(mp.Y)],
-                        boundary_layers=[mp.PML(dpml)])
+sim = mp.Simulation(cell_size=mp.Vector3(sxy, sxy),
+                    geometry=[c1, c2],
+                    sources=[src],
+                    resolution=10,                    
+                    boundary_layers=[mp.PML(dpml)])
+plt.figure(dpi=150)
+sim.plot2D()
+plt.savefig('structureBox.png')
 
-    sim.run(mp.at_beginning(mp.output_epsilon),
-            mp.after_sources(mp.Harminv(mp.Ez, mp.Vector3(r+0.1), fcen, df)),
-            until_after_sources=300)
+sim.run(mp.at_beginning(mp.output_epsilon),
+        mp.after_sources(mp.Harminv(mp.Ez, mp.Vector3(r+0.1), fcen, df)),
+        until_after_sources=300)
 
-    # Output fields for one period at the end.  (If we output
-    # at a single time, we might accidentally catch the Ez field when it is
-    # almost zero and get a distorted view.)
-    sim.run(mp.at_every(1/fcen/20, mp.output_efield_z), until=1/fcen)
+#sim.reset_meep()
+#fcen=0.118
+#df = 0.1
+#sim.sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r+0.1))]
 
-if __name__ == '__main__':
-    main()
+# Start the simulation and get into steady state
+#sim.run(until=600) 
+
+# Prepare the animator and record the steady state response
+#f = plt.figure(dpi=150)
+#Animate = mp.Animate2D(sim, fields=mp.Ez, f=f, realtime=False, normalize=True)
+#sim.run(mp.at_every(0.5,Animate),until=25)
+
+# Close the animator's working frame
+#plt.close()
+
+# Process the animation and view it
+#filename = "ring_simple.mp4"
+#Animate.to_mp4(5,filename)
+#Video(filename)
+
+#sim.reset_meep()
+#fcen=0.147
+#df = 0.1
+#sim.sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r+0.1))]
+#sim.init_sim()
+
+# Start the simulation and get into steady state
+#sim.run(until=500) 
+
+# Prepare the animator and record the steady state response
+#f = plt.figure(dpi=150)
+#Animate = mp.Animate2D(sim, fields=mp.Ez, f=f, realtime=False, normalize=True)
+#sim.run(mp.at_every(0.5,Animate),until=25)
+
+# Close the animator's working frame
+#plt.close()
+
+# Process the animation and view it
+#filename = "ring_mid.mp4"
+#Animate.to_mp4(5,filename)
+#Video(filename)
+
+sim.reset_meep()
+fcen=0.175
+df = 0.1
+sim.sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r+0.1))]
+
+# Start the simulation and get into steady state
+sim.run(until=500) 
+
+# Prepare the animator and record the steady state response
+f = plt.figure(dpi=150)
+Animate = mp.Animate2D(sim, fields=mp.Ez, f=f, realtime=False, normalize=True)
+sim.run(mp.at_every(0.5,Animate),until=25)
+
+# Close the animator's working frame
+plt.close()
+
+# Process the animation and view it
+filename = "ring_large.mp4"
+Animate.to_mp4(5,filename)
+Video(filename)
